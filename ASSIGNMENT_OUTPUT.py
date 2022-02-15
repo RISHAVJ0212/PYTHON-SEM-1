f15280d130ac4038ce88f0f331d4c14855619003
RISHAVJ0212 <97506486+RISHAVJ0212@users.noreply.github.com>
2022-02-15 12:55:11 +0530

    Add files via upload

---
+++ ASSIGNMENT_OUTPUT.py
@@ -0,0 +1,236 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: F:\PYTHON\S_PROGRMAS\ASSIGNMENT_3\ASSIGNMENT_CODE.py ========
+
+
+ ---------------------------------------------------------------------- 
+                                   1                                    
+ ---------------------------------------------------------------------- 
+
+COUNTING THE NUMBER OF WORDS OR LETTERS (in case of single word)
+
+
+Enter the String to count	:	Across the chasm the first sunlight clipped the western echelon of ruined mountains and cones of the badlands volcanoes at the edge of the world and they were gray and red and gold in the moment
+
+
+CHARACTER		 :	No. Of TIMES OCCURED
+
+across 			 :	 1
+the 			 :	 7
+chasm 			 :	 1
+first 			 :	 1
+sunlight 		 :	 1
+clipped 		 :	 1
+western 		 :	 1
+echelon 		 :	 1
+of 			 :	 3
+ruined 			 :	 1
+mountains 		 :	 1
+and 			 :	 4
+cones 			 :	 1
+badlands 		 :	 1
+volcanoes 		 :	 1
+at 			 :	 1
+edge 			 :	 1
+world 			 :	 1
+they 			 :	 1
+were 			 :	 1
+gray 			 :	 1
+red 			 :	 1
+gold 			 :	 1
+in 			 :	 1
+moment 			 :	 1
+
+
+ ---------------------------------------------------------------------- 
+                                   2                                    
+ ---------------------------------------------------------------------- 
+
+FINDING THE NEXT DATE
+
+
+Enter The Date	:	29
+Enter The Month	:	02
+Enter The Year	:	2022
+
+The Date Entered By you is NOT POSSIBLE
+Enter it again
+
+Enter The Date	:	31
+Enter The Month	:	12
+Enter The Year	:	2026
+
+The Date Entered by you is out of range
+Enter the date ranging from 1800 to 2025
+
+Enter The Date	:	29
+Enter The Month	:	02
+Enter The Year	:	2016
+Next date of 29/2/2016 is	:	 1/3/2016
+
+
+ ---------------------------------------------------------------------- 
+                                   3                                    
+ ---------------------------------------------------------------------- 
+
+SQUARING THE NUMBERS
+
+
+Write The Numbers sepearted by a space in them ' ' 
+5 65 15 2 65 1 0 642 494254 428974
+
+The numbers and their Squares are written in form '(NUMBER, SQUARE)' 
+ [(5, 25), (65, 4225), (15, 225), (2, 4), (65, 4225), (1, 1), (0, 0), (642, 412164), (494254, 244287016516), (428974, 184018692676)]
+
+
+ ---------------------------------------------------------------------- 
+                                   4                                    
+ ---------------------------------------------------------------------- 
+
+GET GRADE ON BASIS OG GRADE POINTS
+
+
+Enter the Grade Points	:	11
+
+Enter the Grade Point in range 4 to 10
+
+Enter the Grade Points	:	2
+
+Enter the Grade Point in range 4 to 10
+
+Enter the Grade Points	:	9
+Your Grade is 'A' and Excellent Performance
+
+
+ ---------------------------------------------------------------------- 
+                                   5                                    
+ ---------------------------------------------------------------------- 
+
+REVERSE PYRAMID PATTERN 
+
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+ ---------------------------------------------------------------------- 
+                                   6                                    
+ ----------------------------------------------------------------------  
+
+STORING STUDENTs INFO
+
+
+Enter the Name of the Student	:	RISHAV JINDAL
+Enter the SID of the Student	:	21104095
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	y
+Enter the Name of the Student	:	RISHIT SEHGAL
+Enter the SID of the Student	:	21104103
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	Y
+Enter the Name of the Student	:	RAJAT GARG
+Enter the SID of the Student	:	21104032
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	y
+Enter the Name of the Student	:	VISHNU BANSAL
+Enter the SID of the Student	:	21104040
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	
+
+Enter The Correct Response
+
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	db
+
+Enter The Correct Response
+
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	y
+Enter the Name of the Student	:	DIVIK SATIJA
+Enter the SID of the Student	:	211040130
+Type 'Y' if want Add another Student's Details or Type 'N' if doesnot want to Add	:	n
+
+
+ -------PART A-------
+
+Students details are in form (SID: NAME)
+ {21104095: 'RISHAV JINDAL', 21104103: 'RISHIT SEHGAL', 21104032: 'RAJAT GARG', 21104040: 'VISHNU BANSAL', 211040130: 'DIVIK SATIJA'}
+
+
+ -------PART B-------
+
+Students Details in sorted SIDs form are as Follows:
+ {21104032: 'RAJAT GARG', 21104040: 'VISHNU BANSAL', 21104095: 'RISHAV JINDAL', 21104103: 'RISHIT SEHGAL', 211040130: 'DIVIK SATIJA'}
+
+
+ -------PART C-------
+
+Students Details in sorted NAMEs form are as Follows:
+ {211040130: 'DIVIK SATIJA', 21104032: 'RAJAT GARG', 21104095: 'RISHAV JINDAL', 21104103: 'RISHIT SEHGAL', 21104040: 'VISHNU BANSAL'}
+
+
+ -------PART D-------
+
+
+Enter the SID to search the Name	:	21104032
+
+The NAME of the STUDENT with SID '21104032' is 	:	 RAJAT GARG
+
+
+ ---------------------------------------------------------------------- 
+                                   7                                    
+ ---------------------------------------------------------------------- 
+
+FIBBONACCI SERIES
+
+
+ENTER THE NO. OF TERMS YOU WANT THE FIBONACCI SERIES 	:	100
+
+0       1       1       2       3       5       8       13      21      34      
+55      89      144     233     377     610     987     1597    2584    4181    
+6765    10946   17711   28657   46368   75025   121393  196418  317811  514229  
+832040  1346269 2178309 3524578 5702887 9227465 14930352        24157817        39088169        63245986        
+102334155       165580141       267914296       433494437       701408733       1134903170      1836311903      2971215073      4807526976      7778742049      
+12586269025     20365011074     32951280099     53316291173     86267571272     139583862445    225851433717    365435296162    591286729879    956722026041    
+1548008755920   2504730781961   4052739537881   6557470319842   10610209857723  17167680177565  27777890035288  44945570212853  72723460248141  117669030460994 
+190392490709135 308061521170129 498454011879264 806515533049393 1304969544928657        2111485077978050        3416454622906707        5527939700884757        8944394323791464        14472334024676221       
+23416728348467685       37889062373143906       61305790721611591       99194853094755497       160500643816367088      259695496911122585      420196140727489673      679891637638612258      1100087778366101931     1779979416004714189     
+2880067194370816120     4660046610375530309     7540113804746346429     12200160415121876738    19740274219868223167    31940434634990099905    51680708854858323072    83621143489848422977    135301852344706746049   218922995834555169026   
+
+The Average of FIRST '100' terms of FIBONACI Series is 	:   5.73147844013817e+18
+
+
+ ---------------------------------------------------------------------- 
+                                   8                                    
+ ---------------------------------------------------------------------- 
+ 
+
+GIVEN SETS
+------------
+
+Set1 is 	:	 {1, 2, 3, 4, 5}
+Set2 is 	:	 {8, 2, 4, 6}
+Set3 is 	:	 {1, 17, 5, 9, 13}
+
+
+ -------PART A-------
+
+Set of Elements that are in Set1 and Set2 but not in both	:	 {1, 3, 5, 6, 8}
+
+
+ -------PART B-------
+
+Set of Elements that are exactlyin only Set			:	 {3, 6, 8, 9, 13, 17}
+
+
+ -------PART C-------
+
+Set of Elements that are exactly in two Sets			:	 {1, 2, 4, 5}
+
+
+ -------PART D-------
+
+Set of integers from 1 to 10 that are not in Set1		:	 {6, 7, 8, 9, 10}
+
+
+ -------PART E-------
+
+Set of integers from 1 to 10 that are not in all the three Sets	:	 {10, 7}
